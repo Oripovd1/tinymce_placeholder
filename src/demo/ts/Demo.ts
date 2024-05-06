165f2f9481477488a5d984e600f8149d8593edc1
oripovd1 <oripovd1@gmail.com>
2024-05-06 16:31:06 +0200

    first commit

--- src/demo/ts/Demo.ts
+++ src/demo/ts/Demo.ts
@@ -1,13 +1,15 @@
-import { TinyMCE } from 'tinymce';
+import { TinyMCE } from "tinymce";
 
-import Plugin from '../../main/ts/Plugin';
+import Plugin from "../../main/ts/Plugin";
 
 declare let tinymce: TinyMCE;
 
 Plugin();
 
 tinymce.init({
-  selector: 'textarea.tinymce',
-  plugins: 'code tinymce-placeholder',
-  toolbar: 'tinymce-placeholder'
+  selector: "textarea.tinymce",
+  plugins: "code tinymce-placeholder",
+  toolbar: "tinymce-placeholder",
+  content_style:
+    "span.mce-placeholder{font-size: 14px;user-select:none;} span.mce-placeholder input{display: none;} span.mce-placeholder .mark{padding: 4px 6px; background-color: dimgray; color: #fff;} span.mce-placeholder .txt{padding: 4px 6px; background-color: #eaeaea; color: dimgray;}",
 });
--- src/main/ts/Plugin.ts
+++ src/main/ts/Plugin.ts
@@ -1,16 +1,19 @@
-import { Editor, TinyMCE } from 'tinymce';
+import { Editor, TinyMCE } from "tinymce";
 
 declare const tinymce: TinyMCE;
 
 const setup = (editor: Editor, url: string): void => {
-  editor.ui.registry.addButton('tinymce-placeholder', {
-    text: 'tinymce-placeholder button',
+  const placeholderIcon = `<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-crosshair"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 8v-2a2 2 0 0 1 2 -2h2" /><path d="M4 16v2a2 2 0 0 0 2 2h2" /><path d="M16 4h2a2 2 0 0 1 2 2v2" /><path d="M16 20h2a2 2 0 0 0 2 -2v-2" /><path d="M9 12l6 0" /><path d="M12 9l0 6" /></svg>`;
+  editor.ui.registry.addIcon("placeholder", placeholderIcon);
+  editor.ui.registry.addButton("tinymce-placeholder", {
+    tooltip: "Insert response",
+    icon: "placeholder",
     onAction: () => {
-      editor.setContent('<p>content added from tinymce-placeholder</p>');
-    }
+      editor.insertContent("{{response}}");
+    },
   });
 };
 
 export default (): void => {
-  tinymce.PluginManager.add('tinymce-placeholder', setup);
+  tinymce.PluginManager.add("tinymce-placeholder", setup);
 };
